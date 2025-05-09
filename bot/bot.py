c63a1b8b07f61f200cc659f99871ee9e4d8f1de8
Maria-Del-student <madelitsyna@edu.hse.ru>
2025-05-09 13:21:36 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -62,6 +62,109 @@ def math(message):
 Сайт предмета: https://math-info.hse.ru/2024-25/Дискретная_математика''')
 
 # Языковая игра
+def languagegame(message):
+    bot.send_message(message.from_user.id, '''Привет! В этой игре тебе нужно будет отгадать язык по одному предложению. После каждой неудачной попытки тебе будет предлагаться подсказка. Всего у тебя 3 попытки. 
+Начнём игру?''')
+    bot.register_next_step_handler(message, main)
+def main(message):
+    if message.text.lower() == 'да' or message.text.lower() == 'конечно':
+        all_func = [start_english, start_espanol, start_chinese, start_french, start_japanese, start_korean, start_sanskrit, start_arabic, start_gypsy, start_georgian, start_armenian, start_german, start_polish, start_hebrew, start_hungarian, start_greek, start_finnish, start_russian, start_euskara, start_nahuatl]
+        get_random = random.choice(all_func)
+        return get_random(message)
+    else:
+        bot.send_message(message.from_user.id, 'Хорошо! Хочешь поиграть во что-то другое? Напиши /games')
+def continue_game(message):
+    if message.text.lower() == "да" or message.text.lower() == "конечно":
+        bot.send_message(message.from_user.id, "Напиши /language")
+        bot.register_next_step_handler(message, main)
+    else:
+        bot.send_message(message.from_user.id, 'Спасибо за игру! Пока!')
+
+# Испанский для игры
+def start_espanol(message):
+    bot.send_message(message.from_user.id,"Какой это язык? Hola, amigos!")
+    bot.register_next_step_handler(message, check1_espanol)
+def check1_espanol(message):
+    answer = message.text.lower()
+    if answer == 'испанский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, "Нет. Хочешь подсказку?")
+        bot.register_next_step_handler(message, hint1_espanol)
+def hint1_espanol(message):
+    if message.text.lower() == "да":
+        bot.send_message(message.from_user.id, "Это язык романской группы")
+        bot.register_next_step_handler(message, check2_espanol)
+    else:
+        bot.send_message(message.from_user.id, 'Это испанский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
+def check2_espanol(message):
+    answer = message.text.lower()
+    if answer == 'испанский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, "Нет. Хочешь ещё подсказку?")
+        bot.register_next_step_handler(message, hint2_espanol)
+def hint2_espanol(message):
+    if message.text.lower() == "да":
+        bot.send_message(message.from_user.id, "Это язык Дон Кихота")
+        bot.register_next_step_handler(message, check3_espanol)
+    else:
+        bot.send_message(message.from_user.id, 'Это испанский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
+def check3_espanol(message):
+    answer = message.text.lower()
+    if answer == 'испанский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, 'Нет, это испанский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
+
+# Английский для игры
+def start_english(message):
+    bot.send_message(message.from_user.id,"Какой это язык? Hello, guys!")
+    bot.register_next_step_handler(message, check1_english)
+def check1_english(message):
+    answer = message.text.lower()
+    if answer == 'английский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, "Нет. Хочешь подсказку?")
+        bot.register_next_step_handler(message, hint1_english)
+def hint1_english(message):
+    if message.text.lower() == "да":
+        bot.send_message(message.from_user.id, "Это язык германской группы")
+        bot.register_next_step_handler(message, check2_english)
+    else:
+        bot.send_message(message.from_user.id, 'Это английский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
+def check2_english(message):
+    answer = message.text.lower()
+    if answer == 'английский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, "Нет. Хочешь ещё подсказку?")
+        bot.register_next_step_handler(message, hint2_english)
+def hint2_english(message):
+    if message.text.lower() == "да":
+        bot.send_message(message.from_user.id, "Это язык Шерлока Холмса")
+        bot.register_next_step_handler(message, check3_english)
+    else:
+        bot.send_message(message.from_user.id, 'Это английский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
+def check3_english(message):
+    answer = message.text.lower()
+    if answer == 'английский':
+        bot.send_message(message.from_user.id, "Да! Молодец! Хочешь сыграть еще?")
+        bot.register_next_step_handler(message, continue_game)
+    else:
+        bot.send_message(message.from_user.id, 'Нет, это английский. Хочешь сыграть еще?')
+        bot.register_next_step_handler(message, continue_game)
 
 
 # Виселица
